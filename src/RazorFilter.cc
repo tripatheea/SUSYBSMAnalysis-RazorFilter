aac80b7518eeb5dff337aef5f34bc99340055ed5
Javier <jduarte@caltech.edu>
2014-11-26 04:41:26 +0100

    updating filter

--- src/RazorFilter.cc
+++ src/RazorFilter.cc
@@ -9,18 +9,22 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
 
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackBase.h"
+#include "DataFormats/JetReco/interface/PFJet.h"
+#include "DataFormats/JetReco/interface/PFJetCollection.h"
+#include "DataFormats/METReco/interface/PFMET.h"
+#include "DataFormats/METReco/interface/PFMETCollection.h"
+
 
 #include <iostream>
 #include <string>
 
-#include <TH1.h>
+#include <TTree.h>
 #include <TFile.h>
 
 #include <fstream>
+
+#include "TVector3.h"
+#include "TLorentzVector.h"
 
 class RazorFilter : public edm::EDFilter 
 {
@@ -32,43 +36,41 @@ private:
   virtual void beginJob() ;
   virtual bool filter(edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
- 
-  edm::InputTag muonInputTag_;
+  void computeHemispheres(std::auto_ptr<std::vector<math::XYZTLorentzVector> >& hlist, const std::vector<math::XYZTLorentzVector>& goodJets);
+  static double calcMR(TLorentzVector ja, TLorentzVector jb);
+  static double calcRsq(double MR, TLorentzVector ja, TLorentzVector jb, edm::Handle<reco::PFMETCollection> inputMet);
+      
+  edm::InputTag jetInputTag_;
+  edm::InputTag metInputTag_;
 
-  double binInterval_;
   std::string rootFileName_;
-  double invariantMassMin_;
-  double invariantMassMax_;
+  double minJetPt_;
+  double maxJetEta_;
  
   TFile* rootFile_;
-  TH1D*  invariantMasses_;
+  TTree*  razorTree_;
 
   std::ofstream csvOut_;
   std::string csvFileName_;
 
-  int maxNEvents_;
-  int nEvents_;
 };
 
 RazorFilter::RazorFilter(const edm::ParameterSet& iConfig)
-  : muonInputTag_(iConfig.getParameter<edm::InputTag>("muonInputTag")),
-    binInterval_(iConfig.getParameter<double>("binInterval")),
+  : jetInputTag_(iConfig.getParameter<edm::InputTag>("jetInputTag")),
+    metInputTag_(iConfig.getParameter<edm::InputTag>("metInputTag")),
     rootFileName_(iConfig.getParameter<std::string>("rootFileName")),
-    invariantMassMin_(iConfig.getParameter<double>("invariantMassMin")),
-    invariantMassMax_(iConfig.getParameter<double>("invariantMassMax")),
-    csvFileName_(iConfig.getParameter<std::string>("csvFileName")),
-    maxNEvents_(iConfig.getParameter<int>("maxNEvents")),
-    nEvents_(0)
+    minJetPt_(iConfig.getParameter<double>("minJetPt")),
+    maxJetEta_(iConfig.getParameter<double>("maxJetEta")),
+    csvFileName_(iConfig.getParameter<std::string>("csvFileName"))
 {
   rootFile_ = new TFile(rootFileName_.c_str(), "RECREATE");
 
-  int nbins = int((invariantMassMax_ - invariantMassMin_)/binInterval_); 
   
-  invariantMasses_ = new TH1D("dimuon invariant mass",
-                              "dimuon invariant mass",
-                              nbins, invariantMassMin_, invariantMassMax_);
+  razorTree_ = new TTree("razor tree",
+			 "razor tree");
 
   csvOut_.open(csvFileName_.c_str());
+
 }
 
 
@@ -78,155 +80,57 @@ RazorFilter::~RazorFilter()
 bool
 RazorFilter::filter(edm::Event& event, const edm::EventSetup& eventSetup)
 {
-  edm::Handle<reco::MuonCollection> collection;
-  event.getByLabel(muonInputTag_, collection);
+  edm::Handle<reco::PFJetCollection> jets;
+  event.getByLabel(jetInputTag_, jets);
 
-  if ( ! collection.isValid() )
+  edm::Handle<reco::PFMETCollection> met;
+  event.getByLabel(metInputTag_, met);
+  
+  if ( ! (jets.isValid() && met.isValid()) )
   {
     std::cerr<<"RazorFilter: invalid collection"<<std::endl;
     return false;
   }
   
-  int charge;
-  double pt, eta, phi;
-  double energy, px, py, pz;
-
-  double last_energy = 0.0;
-  double last_px = 0.0;
-  double last_py = 0.0;
-  double last_pz = 0.0;
-
-  double last_pt = 0.0;
-  double last_eta = 0.0;
-  double last_phi = 0.0;
-
-  // Only examine if there are precisely 2 muons in the event (for simplicity)
-  if ( collection->size() != 2 )
+  double pt, eta;
+  int nJets = 0;
+  
+  // Only examine if there are at least 2 jets
+  if ( jets->size() < 2 )
     return false;
 
-  int last_charge = 0;
-  int combined_charge = 0;
-
-  bool last_was_tracker = false;
-  bool last_was_global = false;
-
-  bool this_is_tracker = false;
-  bool this_is_global = false;
- 
-  for ( reco::MuonCollection::const_iterator it = collection->begin(), end = collection->end(); 
-        it != end; ++it) 
+  std::vector<math::XYZTLorentzVector> goodJets;
+   
+  for ( reco::PFJetCollection::const_iterator it = jets->begin(), end = jets->end(); 
+        it != end; ++it)
   {
-    // We are only looking for tracker or global muons, not stand-alone
-    if ( ! ((*it).track().isNonnull() || (*it).combinedMuon().isNonnull()) )
-      return false;
-
-    //if ( nEvents_ >= maxNEvents_ )
-    //  return false;
-    
-    if  ( (*it).combinedMuon().isNonnull() ) // Global muon                                                             
-    {
-      pt = (*it).combinedMuon()->pt();
-      phi = (*it).combinedMuon()->phi();
-      eta = (*it).combinedMuon()->eta();
-    }
-    
-    if ( (*it).track().isNonnull() ) // Tracker muon                                                                      
-    {
-      pt = (*it).track()->pt();
-      phi = (*it).track()->phi();
-      eta = (*it).track()->eta();
-    }
-  
-    charge = (*it).charge();
-    
-    if ( last_charge == 0 ) // i.e. this is the first of the pair of muons
-    {
-      last_charge = charge;
-      last_energy = (*it).energy();
-      last_px = (*it).px();
-      last_py = (*it).py();
-      last_pz = (*it).pz();
-
-      last_pt = pt;
-      last_eta = eta;
-      last_phi = phi;
-
-      if ( (*it).track().isNonnull() )
-      {
-        last_was_global = false;
-        last_was_tracker = true;
-      }
-      
-      if ( (*it).combinedMuon().isNonnull() )
-      {
-        last_was_tracker = false;
-        last_was_global = true;
-      }
-      
-    }
-    
-    else // we are on the second muon of the pair and can compare charge and calculate invariant mass
-    {
-      combined_charge = last_charge*charge;
-      
-      energy = (*it).energy();
-      px = (*it).px();
-      py = (*it).py();
-      pz = (*it).pz();
-
-      double E  = last_energy + energy;
-      double PX = last_px + px;
-      double PY = last_py + py;
-      double PZ = last_pz + pz;
-
-      double m = E*E;
-      m -= (PX*PX + PY*PY + PZ*PZ);
-      m = sqrt(m);
-
-      if ( m < invariantMassMin_ || m > invariantMassMax_ )
-        return false;
-
-      std::string this_type;
-
-      if ( (*it).track().isNonnull() )
-      {
-        this_is_tracker = true;
-        this_is_global = false;
-        this_type = "T";
-      }
-      
-      if ( (*it).combinedMuon().isNonnull() )
-      {
-        this_is_global = true;
-        this_is_tracker = false; // a global actually contains both, so make it false
-        this_type = "G";
-      }
-      
-      std::string last_type;
-
-      if ( last_was_tracker )
-        last_type = "T";
-      if ( last_was_global )
-        last_type = "G";
-
-      // If both are tracker then reject
-      if ( last_was_tracker && this_is_tracker )
-        return false; // TT
-      
-      //invariantMasses_->Fill(m);
-
-      csvOut_<< event.id().run() <<","<< event.id().event() <<","
-	     << last_type <<","
-             << last_energy <<","<< last_px <<","<< last_py <<","<< last_pz <<","
-             << last_pt <<","<< last_eta <<","<< last_phi <<","<< last_charge <<","
-             << this_type <<","
-	     << energy <<","<< px <<","<< py <<","<< pz <<","
-             << pt <<","<< eta <<","<< phi <<","<< charge <<","
-             << m <<std::endl;
+    pt = (*it).pt();
+    eta = (*it).eta();
+    if (pt > minJetPt_ && eta < maxJetEta_){
+      goodJets.push_back((*it).p4());
+      nJets++;
     }
   }
 
-  nEvents_ += 1;
+  // only continue if there are at least 2 jets
+  if (nJets < 2)
+    return false;
+  
+  std::auto_ptr<std::vector<math::XYZTLorentzVector> > hemispheres(new std::vector<math::XYZTLorentzVector> );
+  this->computeHemispheres(hemispheres,goodJets);
+
+  if (hemispheres->size() < 2)
+    return false;
+
+   TLorentzVector ja(hemispheres->at(0).x(),hemispheres->at(0).y(),hemispheres->at(0).z(),hemispheres->at(0).t());
+   TLorentzVector jb(hemispheres->at(1).x(),hemispheres->at(1).y(),hemispheres->at(1).z(),hemispheres->at(1).t());
+
+   double MR = calcMR(ja,jb);
+   double Rsq  = calcRsq(MR,ja,jb,met);
+  
+   
+   csvOut_<< event.id().run() <<","<< event.id().luminosityBlock() <<","<< event.id().event() <<","
+	  << MR <<","<< Rsq << std::endl;
   
   return true;
 }
@@ -234,17 +138,101 @@ RazorFilter::filter(edm::Event& event, const edm::EventSetup& eventSetup)
 void 
 RazorFilter::beginJob()
 {
-  csvOut_<<"Run,Event,Type1,E1,px1,py1,pz1,pt1,eta1,phi1,Q1,Type2,E2,px2,py2,pz2,pt2,eta2,phi2,Q2,M"<<std::endl;
+  csvOut_<<"Run,Lumi,Event,MR,Rsq"<<std::endl;
 }
 
 void 
 RazorFilter::endJob() 
 {
   rootFile_->cd();
-  invariantMasses_->Write();
+  razorTree_->Write();
   rootFile_->Close();
 
   csvOut_.close();
 }
+
+
+
+void
+RazorFilter::computeHemispheres(std::auto_ptr<std::vector<math::XYZTLorentzVector> >& hlist, const std::vector<math::XYZTLorentzVector>& goodJets){
+  using namespace math;
+  using namespace reco;
+  XYZTLorentzVector j1R(0.1, 0., 0., 0.1);
+  XYZTLorentzVector j2R(0.1, 0., 0., 0.1);
+  int nJets = goodJets.size();
+
+  if(nJets<2){ // put empty hemispheres if not enough jets
+    hlist->push_back(j1R);
+    hlist->push_back(j2R);
+    return;
+  }
+  unsigned int N_comb = pow(2,nJets); // compute the number of combinations of jets possible
+  //Make the hemispheres
+  double M_minR = 999999999999.0;
+  unsigned int j_count;
+  for (unsigned int i = 0; i < N_comb; i++) {       
+    XYZTLorentzVector j_temp1, j_temp2;
+    unsigned int itemp = i;
+    j_count = N_comb/2;
+    unsigned int count = 0;
+    while (j_count > 0) {
+      if (itemp/j_count == 1){
+	j_temp1 += goodJets.at(count);
+      } else {
+	j_temp2 += goodJets.at(count);
+      }
+      itemp -= j_count * (itemp/j_count);
+      j_count /= 2;
+      count++;
+    }
+    double M_temp = j_temp1.M2() + j_temp2.M2();
+    if (M_temp < M_minR) {
+      M_minR = M_temp;
+      j1R = j_temp1; 
+      j2R = j_temp2; 
+    }
+  }
+
+  hlist->push_back(j1R);
+  hlist->push_back(j2R);
+  return;
+}
+
+double 
+RazorFilter::calcMR(TLorentzVector ja, TLorentzVector jb){
+  if(ja.Pt()<=0.1) return -1;
+
+  ja.SetPtEtaPhiM(ja.Pt(),ja.Eta(),ja.Phi(),0.0);
+  jb.SetPtEtaPhiM(jb.Pt(),jb.Eta(),jb.Phi(),0.0);
+  
+  if(ja.Pt() > jb.Pt()){
+    TLorentzVector temp = ja;
+    ja = jb;
+    jb = temp;
+  }
+  
+  double A = ja.P();
+  double B = jb.P();
+  double az = ja.Pz();
+  double bz = jb.Pz();
+
+  double MR = sqrt((A+B)*(A+B)-(az+bz)*(az+bz));
+
+  return MR;
+}
+
+double 
+RazorFilter::calcRsq(double MR, TLorentzVector ja, TLorentzVector jb, edm::Handle<reco::PFMETCollection> inputMet){
+  //now we can calculate MTR
+  TVector3 met;
+  met.SetPtEtaPhi((inputMet->front()).pt(),0.0,(inputMet->front()).phi());
+
+  double MTR = sqrt(0.5*(met.Mag()*(ja.Pt()+jb.Pt()) - met.Dot(ja.Vect()+jb.Vect())));
+  
+  //filter events
+  return float(MTR)*float(MTR)/float(MR)/float(MR); //Rsq
+  
+}
+
 
 DEFINE_FWK_MODULE(RazorFilter);
